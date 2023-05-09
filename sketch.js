4f74effa04e8ebe09e9275f7e9d093da3f2242e4
espaulds <112963551+espaulds@users.noreply.github.com>
2023-05-09 00:04:05 -0400

    got background working and homepage links

--- sketch.js
+++ sketch.js
@@ -1,83 +1,89 @@
-//get color palette
-//work out app icons
-//clicking on app icon brings window with project
+var canvas;
+
+let dragging = false;
+let minFrequency = 0.5;
+let maxFrequency = 2;
+let minAmplitude = 0.05;
+let maxAmplitude = 0.5;
+
+let amplitude;
+let frequency;
+const simplex = new SimplexNoise();
 
 
-
-var colors = ['#C3E1EC','#b5dbd8', '#efc1ce', '#eea196', '#f2b799', '#eeca8b', '#afdcc5','#f1ebdd', '#343530'];
-
-function setup() {
-  createCanvas(600, 450);
-  frameRate(1);
-  noFill();
+function setup () {
+  //createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+  
+  mouseX = width / 2;
+  mouseY = height / 2;
 }
 
-function draw() {
-  clear();
+
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
+function draw (){
   background('#C3E1EC');
-  drawScreen(0);
+  
+  const frequency = lerp(minFrequency, maxFrequency, mouseX / width);
+  const amplitude = lerp(minAmplitude, maxAmplitude, mouseY / height);
+  
+  const dim = Math.min(width, height);
   
   
+  noFill();
+  stroke(0);
+  strokeWeight(dim * 0.0025);
   
-  
+  const time = millis() / 6000;
+  const rows = 10;
 
-  noLoop();
-}
-
-function drawScreen(start) {
-  fill('#EFF1F3')
-  rect(start+60, start+20, 360, 400, 15);
-  fill('#dbddde');
-  rect(start+60, start+20, 360, 28,15, 15, 0, 0);
-  rect(start+60, start+48, 360, 20);
-  fill('#EFF1F3')
-  rect(start+100, start+52, start+315, start+12,15, 15, 15, 15);
-  fill('#eea196');
-  circle(start+75, start+34, 10);
-  fill('#ece7c3');
-  circle(start+95, start+34, 10);
-  fill('#e1ecc3');
-  circle(start+115, start+34, 10);
   
-  fill('#000000');
-  textSize(9);
-  text('https://aboutcoder.com', start+105, start+61);
-  
-  fill('#000000');
-  textSize(20);
-  text('Elizabeth Spaulding', start+65, start+105);
-  textSize(12);
-  text('project description: what you hoped to gain/big picture', start+70, start+155);
-  text('methods: programming methods, throught process, approach, how you did it', start+70, start+190, start+300);
-  text('obstacles & solutions', start+70, start+255);
-  text('conclusions', start+70, start+305);
-  
-  
-  
-}
-
-function icons(){
-  
-}
-
-function otherStuff(){
-  background(220);
-  var margin = 0;
-  var width;
-  var height;
-  var space = 0;
-  var numSquares = 2;
-  var radius = width/(2*numSquares);
-  
-  translate(margin, margin);
-  for(var y = radius; y<height; y = y + 2*radius){
-    for(var x = radius; x<width; x = x + 2*radius){
-      let c = color(colors[int(random(0, colors.length))]);
-      let s = color('#00000000');
-      stroke(s);
-      
-      fill(c);
-      circle(x,y,2*radius);
-    }
+  for (let y = 0; y < rows; y++) {
+    const v = rows <= 1 ? 0.5 : y / (rows - 1);
+    const py = v * height;
+    drawNoiseLine({
+      v,
+      start: [ 0, py ],
+      end: [ width, py ],
+      amplitude: amplitude * height,
+      frequency,
+      time: time * 0.5,
+      steps: 150
+    });
   }
 }
+
+function drawNoiseLine (opt = {}) {
+  const {
+    v,
+    start,
+    end,
+    steps = 10,
+    frequency = 1,
+    time = 0,
+    amplitude = 1
+  } = opt;
+  
+  const [ xStart, yStart ] = start;
+  const [ xEnd, yEnd ] = end;
+  beginShape();
+  for (let i = 0; i < steps; i++) {
+    const t = steps <= 1 ? 0.5 : i / (steps - 1);
+
+    const x = lerp(xStart, xEnd, t);
+    
+    let y = lerp(yStart, yEnd, t);
+    
+    y += (simplex.noise3D(t * frequency + time, v * frequency, time)) * amplitude;
+    
+    vertex(x, y);
+  }
+  endShape();
+}
+
